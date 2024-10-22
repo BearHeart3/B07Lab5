1b3f3fed5f6fa0fe073a6581704d14ac15e96294
Saheda Vachhiat <saheda.vachhiat0909@gmail.com>
2024-10-22 13:57:56 -0400

    Add Square.java

---
+++ src/lab4/Square.java
@@ -0,0 +1,38 @@
+package lab4;
+
+public class Square{
+	  Point A; 
+	  Point B; 
+	  Point C; 
+	  Point D; 
+	  
+	
+	  public Square (Point A, Point B, Point C, Point D){
+		  this.A = A; 
+		  this.B = B; 
+		  this.C = C;
+		  this.D = D; 
+
+	  }
+
+	public double perimeter(){
+	  return A.distance(B) + B.distance(C) + C.distance(D) + D.distance(A); 
+
+	}
+	
+
+	public boolean isSquare(){
+		
+		double dAB = A.distance(B); 
+	    double dBC = B.distance(C); 
+	    double dCD = C.distance(D); 
+	    double dDA = D.distance(A); 
+
+	    double diagonalAC = A.distance(C); // Diagonal from A to C
+	    double diagonalBD = B.distance(D); // Diagonal from B to D
+ 
+
+	  return (dAB == dBC && dBC == dCD && dCD == dDA && diagonalAC == diagonalBD);
+
+	}
+	}
