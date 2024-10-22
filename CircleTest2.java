85e208e076a36eeb2c0d854d9337468aa870c4aa
BearHeart3 <helenyunhan.hu@mail.utoronto.ca>
2024-10-22 16:32:10 -0400

    created CircleTest

---
+++ CircleTest2.java
@@ -0,0 +1,21 @@
+package lab4
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CircleTest2 {
+
+  @Test
+  void testArea() {
+        Circle radius = new Circle(3);
+        assertEquals(28.2743, radius.area(), 0.0001);
+    }
+
+  @Test
+  void testPerimeter() {
+        Circle radius = new Circle(3);
+        assertEquals(18.8496, radius.perimeter(), 0.0001);
+    }
+
+}
